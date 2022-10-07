45827fde50c995abfc448e7da25e67e2cb1188a6
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-10-08 03:15:36 +0500

    add product categories

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const categories = [
+    {
+        id: 1,
+        img: `https://images.unsplash.com/photo-1600574691453-499962cc0611?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
+        title: 'Shirt Style'
+    },
+    {
+        id: 2,
+        img: `https://images.unsplash.com/photo-1520965291898-92731ff2f8ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
+        title: 'Longwear Love'
+    },
+    {
+        id: 3,
+        img: `https://images.unsplash.com/photo-1561175474-c25951d8bf9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
+        title: 'Light Jackets'
+    }
+]
