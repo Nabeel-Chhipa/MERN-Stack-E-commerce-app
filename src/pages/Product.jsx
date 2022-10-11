42ca3c3833c8e7d562ad8fc13ed3d32bc31e38af
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-10-12 03:12:57 +0500

    add single product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,168 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faminus, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 30px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 0px;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+`;
+
+const Amount = styled.span`
+  width: 50px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid teal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 15px;
+`;
+
+const Button = styled.button`
+  background-color: #fff;
+  border: 1px solid teal;
+  padding: 7px;
+  width: 45%;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #cfcece4a !important;
+    transition all .9s ease-in-out
+  }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImageContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImageContainer>
+        <InfoContainer>
+          <Title>Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            sequi quo dolores deserunt, voluptatibus corrupti suscipit
+            repellendus quidem! Nam, itaque?
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <FontAwesomeIcon icon="fa-solid fa-minus" />
+              <Amount>1</Amount>
+              <FontAwesomeIcon icon="fa-solid fa-plus" />
+            </AmountContainer>
+            <Button>Add To Cart</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
